a12ae46e1c799bb6db86ebeb084888d5578ab21e
cangtianhuang <l1903374751@gmail.com>
2025-07-09 12:38:13 +0800

    fix det singular

--- tester/api_config/config_analyzer.py
+++ tester/api_config/config_analyzer.py
@@ -784,7 +784,15 @@ class TensorConfig:
                         self.numpy_tensor = numpy.random.randint(1, M + 1, size=self.shape).astype(self.dtype)
                 elif api_config.api_name.endswith("pca_lowrank"):
                     self.numpy_tensor = numpy.random.randn(*self.shape).astype(self.dtype)
-                    
+                elif api_config.api_name.endswith("det"):
+                    if self.check_arg(api_config, 0, "x"):
+                        assert len(self.shape) >= 2, "Input must be at least 2D."
+                        assert self.shape[-1] == self.shape[-2], "Input must be square matrices."
+                        n = self.shape[-1]
+                        A = numpy.random.uniform(low=0.5, high=1.0, size=self.shape).astype(self.dtype)
+                        A_T = numpy.swapaxes(A, -1, -2)
+                        self.numpy_tensor = numpy.matmul(A, A_T) + numpy.eye(n, dtype=self.dtype)
+
             elif api_config.api_name == "paddle.linspace":
                 if "int" in self.dtype:
                     self.numpy_tensor = (numpy.random.randint(0, 65535, size=self.shape)).astype(self.dtype)
