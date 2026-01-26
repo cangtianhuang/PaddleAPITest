d2e829712125c4d00c8f30ef46109a9594b2948c
Zhan Rongrui <46243324+zrr1999@users.noreply.github.com>
2026-01-26 15:35:42 +0800

    add more slice case (#609)

--- tester/api_config/config_analyzer.py
+++ tester/api_config/config_analyzer.py
@@ -2480,15 +2480,18 @@ class TensorConfig:
                     arr = self.get_arg(api_config, 0, "arr")
                     value = self.get_arg(api_config, 2, "value")
                     min_dim = min(arr.shape)
-                    if value is not None and hasattr(value, "shape"):
-                        indices = numpy.zeros(self.numel(), dtype="int64")
-                        num_true = min(value.shape[0], self.numel())
-                        true_indices = numpy.random.choice(
-                            self.numel(), size=num_true, replace=False
-                        )
-                        indices[true_indices] = 1
+                    if self.dtype == "bool":
+                        if value is not None and hasattr(value, "shape"):
+                            indices = numpy.zeros(self.numel(), dtype="int64")
+                            num_true = min(value.shape[0], self.numel())
+                            true_indices = numpy.random.choice(
+                                self.numel(), size=num_true, replace=False
+                            )
+                            indices[true_indices] = 1
+                        else:
+                            indices = numpy.random.choice([0, 1], size=self.numel())
                     else:
-                        indices = numpy.random.choice([0, 1], size=self.numel())
+                        indices = numpy.random.randint(0, min_dim, size=self.numel())
                     self.numpy_tensor = indices.reshape(self.shape).astype(self.dtype)
 
             elif api_config.api_name == "paddle.poisson":
